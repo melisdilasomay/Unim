6aa69a55f83121c931df7587f9e99706916c0854
Melis Dila Somay <melisdilasomay@gmail.com>
2016-05-11 15:57:33 +0300

    Create items.py

---
+++ collectdata/items.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+
+class MuglaItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+    comment = scrapy.Field()
+    school =scrapy.Field()
